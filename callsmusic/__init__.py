91038cfcad7b17589b16ce6d766e21e8bbfe4f0b
karanveersinghA2 <109973534+karanveersinghA2@users.noreply.github.com>
2022-07-28 23:49:23 +0530

    Update __init__.py

--- callsmusic/__init__.py
+++ callsmusic/__init__.py
@@ -1,4 +1,4 @@
-# Powered by @HYPER_AD13 | @ShiningOff
+# Powered by @Dynamite_101 | @hehe_joker
 # Dear Pero ppls Plish Don't remove this line from here🌚
 
 from .callsmusic import pytgcalls, run, client
